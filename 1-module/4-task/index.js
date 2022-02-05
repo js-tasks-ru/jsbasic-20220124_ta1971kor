8ee49dd6c0cb06f21142a496ef03e6d9992048b6
tatikio <ta1971kor@gmail.com>
2022-02-05 18:44:04 +0700

    task 1-4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,15 @@
 function checkSpam(str) {
-  // ваш код...
+    let strLower = str.toLowerCase();
+    let spam1 = '1xBet';
+    let spam2 = 'XXX';
+    let spam1Lower = spam1.toLowerCase();
+    let spam2Lower = spam2.toLowerCase();
+    if (strLower.indexOf(spam1Lower) !== -1) {
+        return true;
+    }
+    if (strLower.indexOf(spam2Lower) !== -1) {
+        return true;
+    }
+    return false;
 }
+

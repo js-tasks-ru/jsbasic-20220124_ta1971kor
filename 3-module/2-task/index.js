e2f72c5586dab7ee9a0c339685aa88729afb6086
tatikio <ta1971kor@gmail.com>
2022-02-09 23:28:49 +0700

    task 3-2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        if (num >= a && num <= b) {
+            result.push(num);
+        }
+    }
+    return result;
 }

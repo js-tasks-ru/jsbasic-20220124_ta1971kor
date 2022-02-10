162f05b20759f88c14900a5bd564ec4958806941
tatikio <ta1971kor@gmail.com>
2022-02-10 22:53:15 +0700

    task 3-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,21 @@
 function camelize(str) {
-  // ваш код...
+    let result = "";
+    let parts = str.split("-");
+    for (let i = 0; i < parts.length; i++) {
+        let part = parts[i];
+        if (i === 0) {
+            result = result + part;
+        } else {
+            let upper = ucFirst(part);
+            result = result + upper;
+        }
+    }
+    return result;
+}
+function ucFirst(str) {
+    if (str === '') {
+        return '';
+    }
+    let strUpper = str.toUpperCase();
+    return strUpper[0] + str.slice(1);
 }

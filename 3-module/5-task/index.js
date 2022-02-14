abd347d88f01ed93e6c139db28d2d53ea1dfb882
tatikio <ta1971kor@gmail.com>
2022-02-14 22:42:39 +0700

    task 3-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,38 @@
 function getMinMax(str) {
-  // ваш код...
+    let parts = str.split(" ");
+    let numbers = [];
+    for (let i = 0; i < parts.length; i++) {
+        let part = parts[i];
+        let partIsNumeric = isNumeric(part);
+        if (partIsNumeric === true) {
+            numbers.push(+part);
+        }
+    }
+    let currentMin;
+    let currentMax;
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        if (currentMin === undefined) {
+            currentMin = number;
+        } else {
+            currentMin = Math.min(currentMin, number);
+        }
+        if (currentMax === undefined) {
+            currentMax = number;
+        } else {
+            currentMax = Math.max(currentMax, number);
+        }
+    }
+    return {
+        min: currentMin,
+        max: currentMax,
+    }
+}
+
+function isNumeric(n) {
+    if (n === "") {
+        return false;
+    }
+    let attempt = +n;
+    return isFinite(attempt);
 }
